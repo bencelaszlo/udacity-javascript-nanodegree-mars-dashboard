c57f9870931c5d6e23c78a6a9241eab4a786dcf2
bencelaszlo <bencelaszlo@protonmail.com>
2021-04-18 03:20:13 +0200

    Implement basic functionality.
    On branch project
    
    Changes to be committed:
    	deleted:    ../../.env-sample
    	new file:   ../../package-lock.json
    	modified:   ../../package.json
    	new file:   .eslintrc.js
    	modified:   assets/stylesheets/index.css
    	modified:   client.js
    	modified:   index.html
    	new file:   ../server/.eslintrc.js
    	modified:   ../server/index.js

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,105 +1,129 @@
-let store = {
-    user: { name: "Student" },
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+const store = {
+  rovers: undefined,
+  roverImages: undefined,
+  roverImageIndex: 0,
+  previousRover: undefined,
+  port: 3001,
+  selectedRover: undefined
 }
 
-// add our markup to the page
 const root = document.getElementById('root')
 
 const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
+  store = Object.assign(store, newState)
+  render(root, store)
 }
 
 const render = async (root, state) => {
-    root.innerHTML = App(state)
+  root.innerHTML = await App(state)
 }
 
-
-// create content
-const App = (state) => {
-    let { rovers, apod } = state
-
-    return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
+const App = async (state) => {
+  return `
+        ${Header(state)}
+        ${await RoverImage(state)}
         <footer></footer>
     `
 }
 
 // listening for load event because page should load before any JS is called
-window.addEventListener('load', () => {
-    render(root, store)
+window.addEventListener('load', async () => {
+  await getRovers(store)
+  store.selectedRover = store.rovers.find(rover => rover.name === 'Curiosity')
+  store.previousRover = store.selectedRover
+  await getRoverImage(store)
+  await render(root, store)
+  await getRoverImage(store)
 })
 
 // ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
-
-    return `
-        <h1>Hello!</h1>
-    `
+const Header = (state) => {
+  return `
+      <header>
+          <h1 id="header-title">Mars Dashboard</h1>
+          <button id="tablink1" class="tablink" onclick="selectRover('curiosity')">Curiosity</button>
+          <button id="tablink2" class="tablink" onclick="selectRover('opportunity')">Opportunity</button>
+          <button id="tablink3" class="tablink" onclick="selectRover('spirit')">Spirit</button>
+      </header>
+      `
 }
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
+const RoverImage = async (state) => {
+  const { roverImages, roverImageIndex, selectedRover, previousRover } = state
 
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
+  if (!selectedRover) {
+    return ''
+  }
 
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
+  if (!roverImages || selectedRover !== previousRover) {
+    await getRoverImage(store)
+    store.previousRover = selectedRover
+    store.roverImagesCurrentIndex = 0
+  }
 
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
+  if (roverImages) {
+    return `
+      <div class="wrapper">
+        <div id="left" onclick="swipeLeft(store)">Left</div>
+        <div id="image" style="background-image: url('${roverImages[roverImageIndex].img_src}'); background-repeat: no-repeat; backgroud-clip: content-box; background-position: center center; background-size: cover;" ></div>
+        <div id="right" onclick="swipeRight(store)">Right</div>
+      </div>`
+  }
+}
+
+const selectRover = (roverName) => {
+  if (roverName === store.previousRover) {
+    return
+  }
+
+  updateStore(store, { roverName })
+
+  setTimeout(() => {
+    const selectedRover = store.rovers.find(rover => rover.name.toLowerCase() === store.roverName)
+    updateStore(store, { selectedRover })
+  }, 0)
+}
+
+const swipeLeft = (state) => {
+  let { roverImageIndex } = state
+
+  if (roverImageIndex > 0) {
+    roverImageIndex--
+    updateStore(store, { roverImageIndex })
+  } else {
+    document.getElementById('left').style.backgroundColor = '#ccc'
+  }
+}
+
+const swipeRight = (state) => {
+  let { roverImages, roverImageIndex } = state
+
+  if (roverImageIndex < roverImages.length - 1) {
+    roverImageIndex++
+    updateStore(store, { roverImageIndex })
+  } else {
+    document.getElementById('right').style.backgroundColor = '#ccc'
+  }
 }
 
 // ------------------------------------------------------  API CALLS
 
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
+const getRoverImage = async (state) => {
+  let { roverImages, selectedRover } = state
+  const maxDate = selectedRover.max_date
 
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+  if (maxDate && maxDate !== 'undefined') {
+    const roverImageData = await fetch(`http://localhost:${state.port}/rover/${selectedRover.name}?maxDate=${maxDate}`)
+    roverImages = await roverImageData.json()
+    updateStore(store, { roverImages })
+  }
+}
 
-    return data
+const getRovers = async (store) => {
+  let { rovers } = store
+
+  const roverData = await fetch(`http://localhost:${store.port}/rovers`)
+  rovers = await roverData.json()
+  updateStore(store, { rovers })
 }
--- project/src/server/index.js
+++ project/src/server/index.js
@@ -4,25 +4,43 @@ const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const path = require('path')
 
+require('dotenv').config()
+
 const app = express()
-const port = 3000
+const port = 3001
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-// your API calls
+const filterRovers = (roversData) => {
+  const roverNames = ['curiosity', 'opportunity', 'spirit']
+  return roversData.filter(rover => roverNames.includes(rover.name.toLowerCase()))
+}
 
-// example API call
-app.get('/apod', async (req, res) => {
-    try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
-        console.log('error:', err);
-    }
+app.get('/rovers', async (req, res) => {
+  try {
+    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=${process.env.API_KEY}`
+    const rovers = await fetch(url)
+    const roversJson = await rovers.json()
+    const filteredRovers = filterRovers(roversJson.rovers)
+    res.send(filteredRovers)
+  } catch (err) {
+    console.log('error: ', err)
+  }
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/rover/:name', async (req, res) => {
+  try {
+    const name = req.params.name
+    const maxDate = req.query.maxDate
+    const data = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${name}/photos?earth_date=${maxDate}&api_key=${process.env.API_KEY}`).then(res => res.json())
+    res.send(data.photos)
+  } catch (err) {
+    console.log('error: ', err)
+    res.status(500).send(err)
+  }
+})
+
+app.listen(port, () => console.log(`Mars Dashboard app listening on port ${port}.`))
